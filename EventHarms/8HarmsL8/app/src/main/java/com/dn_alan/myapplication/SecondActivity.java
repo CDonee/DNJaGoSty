c02096c755d683ef55fa3316be083525ca0e7419
18047091 <870419665@qq.com>
2022-12-11 20:58:49 +0800

    some code for read !

---
+++ EventHarms/8HarmsL8/app/src/main/java/com/dn_alan/myapplication/SecondActivity.java
@@ -0,0 +1,41 @@
+package com.dn_alan.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.dn_alan.myapplication.core.DNEventbus;
+import com.dn_alan.myapplication.core.Hermes;
+import com.dn_alan.myapplication.service.HermesService;
+
+import org.greenrobot.eventbus.EventBus;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Hermes.getDefault().connect(this, HermesService.class);
+    }
+
+    public void send(View view) {
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                EventBus.getDefault().post(new Friend("Alan", 18));
+//            }
+//        }).start();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+                DNEventbus.getDefault().post(new Friend("Alan", 18));
+//            }
+//        }).start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
